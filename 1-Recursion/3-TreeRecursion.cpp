67793c755610347de38921e5866f02459c953bb5
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-31 01:22:26 +0530

    Create 3-TreeRecursion.cpp

---
+++ 1-Recursion/3-TreeRecursion.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fun(int n)
+{
+
+    if (n > 0)
+    {
+        cout << n;
+        fun(n - 1);
+        fun(n - 1);
+    }
+    return 0;
+}
+
+int main()
+{
+    fun(3);
+    return 0;
+}
