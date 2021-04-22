27b59e7988d77b5557ddeb15d3e3d6a438a5b420
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-04-23 04:11:24 +0530

    Create 14-Fibonacci.cpp

---
+++ 1-Recursion/14-Fibonacci.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fib(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
+int F[10];
+
+int mfib(int n)
+{
+    if (n <= 1)
+    {
+        F[n] = n;
+        return n;
+    }
+    else
+    {
+        if (F[n - 2] == -1)
+        {
+            F[n - 2] = mfib(n - 2);
+        }
+        if (F[n - 1] == -1)
+        {
+            F[n - 1] = mfib(n - 1);
+        }
+        F[n] = F[n - 2] + F[n - 1];
+        return F[n - 2] + F[n - 1];
+    }
+}
+
+int main()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        F[i] = -1;
+    }
+    cout << fib(6);
+    cout << mfib(6);
+    return 0;
+}
