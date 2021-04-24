ca332d92f74517d1367075b5a890dd0d85895958
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-04-24 22:22:37 +0530

    Create 1-ArrayADT.cpp

---
+++ 2-Arrays/1-ArrayADT.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Array
+{
+    int *A;
+    int size;
+    int length;
+};
+
+void Display(struct Array arr)
+{
+    cout << "Elements are";
+    for (int i = 0; i < arr.length; i++)
+    {
+        cout << arr.A[i] << ' ';
+    }
+}
+
+int main()
+{
+    struct Array arr;
+    int n;
+    cout << "Enter size of an array";
+    cin >> arr.size;
+    arr.A = new int[arr.size];
+    arr.length = 0;
+
+    cout << "Enter number of numbers";
+    cin >> n;
+
+    cout << "Enter all elements";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr.A[i];
+    }
+    arr.length = n;
+
+    Display(arr);
+
+    return 0;
+}
