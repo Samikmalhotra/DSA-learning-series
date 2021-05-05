49241b93b345a130674fa5b3bbbec51f491483d7
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-05-05 12:00:31 +0530

    Update 9-IsSortArray.cpp

--- 2-Arrays/9-IsSortArray.cpp
+++ 2-Arrays/9-IsSortArray.cpp
@@ -1,6 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void swap(int &a, int &b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+void Rearrange(int a[], int length)
+{
+    int i = 0;
+    int j = length - 1;
+
+    while (i < j)
+    {
+        while (a[i] < 0)
+            i++;
+        while (a[i] > 0)
+            j--;
+        if (i < j)
+        {
+            swap(a[i], a[j]);
+        }
+    }
+}
+
 void InsertSort(int a[], int length, int x)
 {
     int i = length - 1;
@@ -12,13 +38,29 @@ void InsertSort(int a[], int length, int x)
     a[i + 1] = x;
     length++;
 }
+
+int isSorted(int a[], int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        if (a[i] > a[i + 1])
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 int main()
 {
-    int A[] = {2, 4, 6, 8, 9};
+    int A[] = {2, 4, -4, -7, 6, 8, 9};
     int length = (sizeof(A) / sizeof(A[0]));
-    InsertSort(A, length, 5);
+    // InsertSort(A, length, 5);
+    Rearrange(A, length);
+
     for (int j = 0; j < length; j++)
     {
         cout << A[j] << ' ';
     }
+    cout << isSorted(A, length);
 }
