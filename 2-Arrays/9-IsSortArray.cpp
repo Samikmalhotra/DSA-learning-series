a60d4fb8590196c129c84adb55cae6ebf0ad3971
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-05-05 11:50:51 +0530

    Update 9-IsSortArray.cpp

--- 2-Arrays/9-IsSortArray.cpp
+++ 2-Arrays/9-IsSortArray.cpp
@@ -4,21 +4,21 @@ using namespace std;
 void InsertSort(int a[], int length, int x)
 {
     int i = length - 1;
-    length++;
     while (i >= 0 && a[i] > x)
     {
         a[i + 1] = a[i];
         i--;
     }
     a[i + 1] = x;
+    length++;
 }
 int main()
 {
     int A[] = {2, 4, 6, 8, 9};
     int length = (sizeof(A) / sizeof(A[0]));
     InsertSort(A, length, 5);
-    for (int i = 0; i < length; i++)
+    for (int j = 0; j < length; j++)
     {
-        cout << A[i] << ' ';
+        cout << A[j] << ' ';
     }
 }
